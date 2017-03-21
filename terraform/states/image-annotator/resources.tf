e0a5d5585bb441550c28113a513c3bbbd588efe0
Tyler Kellen <tyler@sleekcode.net>
2017-03-21 18:07:51 -0400

    add public read policy to image annotator bucket

--- terraform/states/image-annotator/resources.tf
+++ terraform/states/image-annotator/resources.tf
@@ -15,4 +15,26 @@ module "subnet" {
 resource "aws_s3_bucket" "main" {
   acl = "public-read"
   bucket = "image-annotator-assets"
+  policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicReadGetObject",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": [
+        "s3:GetObject",
+        "s3:ListBucket"
+      ],
+      "Resource": [
+        "arn:aws:s3:::image-annotator-assets",
+        "arn:aws:s3:::image-annotator-assets/*"
+      ]
+    }
+  ]
+}
+EOF
 }
