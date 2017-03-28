d02332fb5cf6d675ca98575c1a2e927630a37c6c
Tyler Kellen <tyler@sleekcode.net>
2017-03-28 17:08:35 -0400

    add ajl.ai

--- terraform/states/ajl/dns.tf
+++ terraform/states/ajl/dns.tf
@@ -12,6 +12,13 @@ resource "aws_route53_zone" "fightbias-io" {
   }
 }
 
+resource "aws_route53_zone" "ajl-ai" {
+  name = "ajl.ai"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
 resource "aws_route53_record" "fightbias-io_A_fightbias-io" {
   zone_id = "${aws_route53_zone.fightbias-io.id}"
   type = "A"
@@ -24,6 +31,14 @@ resource "aws_route53_record" "fightbias-org_A_fightbias-org" {
   zone_id = "${aws_route53_zone.fightbias-org.id}"
   type = "A"
   name = "fightbias.org"
+  ttl = "1"
+  records = ["${aws_instance.production.public_ip}"]
+}
+
+resource "aws_route53_record" "ajl-ai_A_ajl-ai" {
+  zone_id = "${aws_route53_zone.ajl-ai.id}"
+  type = "A"
+  name = "ajl.ai"
   ttl = "1"
   records = ["${aws_instance.production.public_ip}"]
 }
